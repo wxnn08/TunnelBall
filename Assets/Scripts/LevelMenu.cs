54f73696f36d267d5fb17b3db664cc14a37f6c37
Bryan Valeriano <bryan.b.valeriano@gmail.com>
2021-08-14 21:58:10 -0300

    Level Menu

---
+++ Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectMenu : MonoBehaviour
+{
+	public void Level1() {
+	//	SceneManager.LoadScene("Level1Scene");
+	}
+
+	public void Level2() {
+	//	SceneManager.LoadScene("Level2Scene");
+	}
+
+	public void Level3() {
+	//	SceneManager.LoadScene("Level3Scene");
+	}
+}
